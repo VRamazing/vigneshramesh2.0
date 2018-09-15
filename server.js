ee4e7ce18ad544da4974fa9537abd61dc8b4cca8
techieVignesh <r.vignesh149@gmail.com>
2018-09-15 14:57:43 +0530

    Download resume functionality added via api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const hbs = require("hbs");
 const data = require("./public/scripts/data")
+const app = express();
+const path = require("path")
 
-var app = express();
+// app.use('/public', express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'hbs');
 
@@ -54,6 +56,21 @@ app.get("/art", (req, res) => {
 
 app.get("/bad", (req, res) => {
 	res.send("<h3>Unable to handle request</h3>");
+});
+
+app.get('/resume/:id/download', function (req, res, next) {
+    if(req.params.id == "game"){
+      var filePath = __dirname + "/public/resume/resume_game.pdf";
+    }else if(req.params.id == "full_stack"){
+      var filePath = __dirname + "/public/resume/resume_full_stack.pdf";
+    }
+    res.download(filePath, 'resume_vignesh_ramesh.pdf', function(err){
+      if (err) {
+        console.log(err);
+      } else{
+        console.log("Thanks for downloading.")
+      }
+    });  
 });
 
 app.listen(3000);
