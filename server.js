80d6d9529f17de53fe55b3c684651be42579eadb
vignesh r <r.vignesh149@gmail.com>
2024-05-03 20:59:51 +0530

    Updated website structure

--- server.js
+++ server.js
@@ -15,64 +15,17 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.get("/", (req, res) => {
-  res.render('index.hbs', {techStack: data.techStack})
+  res.render('index.hbs', {techStack: data.techStack, testimonials: data.testimonials})
 });
 
 app.get("/web-apps", (req, res) => {
   res.render('projects.hbs', {projects: data.web_apps, category_name: "web apps"});
 });
 
-app.get("/games", (req, res) => {
-  res.render('projects.hbs', {projects: data.games, category_name: "games"});
-});
-
-app.get("/blog", (req, res) => {
-  res.render('blog.hbs')
-});
-
-app.get("/testimonials", (req, res) => {
-  res.render('testimonials.hbs', {testimonials: data.testimonials});
-});
-
 app.get("/contact", (req, res) => {
   res.render('contact.hbs')
 });
 
-app.get("/music", (req, res) => {
-  res.render('music.hbs' )
-});
-
-app.get("/logos", (req, res) => {
-  logosData = _.shuffle(data.logos)
-  res.render('logos.hbs', {logos: logosData, length: logosData.length});
-});
-
-app.get("/artwork", (req, res) => {
-  artsData = _.shuffle(data.artwork);
-  totalArts = artsData.length
-  //convert artwork into 4 equal sized array;
-  var artworkPerColumns = [[],[],[],[]];
-  var index = 0;
-  for(var i=0; i<totalArts; i++){
-    if(i == 0 || i%4==0){
-      artworkPerColumns[0].push(artsData[i])
-    }
-    if(i%4==1){
-      artworkPerColumns[1].push(artsData[i])
-    }
-    if(i%4==2){
-      artworkPerColumns[2].push(artsData[i])
-    }
-    if(i%4==3){
-      artworkPerColumns[3].push(artsData[i])
-    }
-  }
-  res.render('art.hbs', {artwork: artworkPerColumns, length: totalArts});
-});
-
-app.get("/bad", (req, res) => {
-	res.send("<h3>Unable to handle request</h3>");
-});
 
 app.get('/resume', function (req, res, next) {
     var filePath = __dirname + "/public/resume/resume_full_stack.pdf";
